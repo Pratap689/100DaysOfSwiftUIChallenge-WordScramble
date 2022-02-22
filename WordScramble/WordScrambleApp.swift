cf3baadf6e6ad015c8532e31351db60a3c3657a4
Pratap689 <pratapapptunix@gmail.com>
2022-02-22 22:13:33 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Pratap Rana on 22/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
